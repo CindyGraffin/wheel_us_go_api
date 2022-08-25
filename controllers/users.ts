32bee97bd4e32783262ee22a3a3d64dac4762039
CindyGraffin <graffincindy@gmail.com>
2022-08-25 14:44:51 +0200

    minor fix

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,9 +1,9 @@
-import { User } from "../models/User";
+import { UserModel } from "../models/User";
 import { NextFunction, Request, Response } from "express";
 
 export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const user = await User.findById(req.params.id)
+        const user = await UserModel.findById(req.params.id)
         res.status(200).json(user)
     } catch (error) {
         next(error); 
@@ -12,7 +12,7 @@ export const getUserById = async (req: Request, res: Response, next: NextFunctio
 
 export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const users = await User.find()
+        const users = await UserModel.find()
         res.status(200).json(users)
     } catch (error) {
         next(error); 
