45e0a188f95ef614168cecde399a0acc5ea15616
CindyGraffin <graffincindy@gmail.com>
2022-09-21 16:57:49 +0200

    fix conflicts

--- models/User.ts
+++ models/User.ts
@@ -3,7 +3,6 @@ import { UserDto } from "../dtos/users.dto";
 
 const { Schema } = mongoose;
 
-<<<<<<< Updated upstream
 const UserSchema = new Schema<UserDto>(
     {
         firstname: {
@@ -78,69 +77,7 @@ const UserSchema = new Schema<UserDto>(
     },
     { timestamps: true }
 );
-=======
-const UserSchema = new Schema<User>({
-    firstname: {
-        type: String,
-        required: true
-    },
-    lastname: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    birthday: {
-        type: Date,
-        required: true
-    },
-    role: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    city: {
-        type: String,
-        required: true
-    },
-    userImg: {
-        type: String
-    },
-    outingPart: {
-        type: Number,
-        required: true
-    },
-    outingCre: {
-        type: Number,
-        required: true
-    },
-    roomsId: [
-        {
-            type: mongoose.Types.ObjectId,
-            ref: 'Room'
-        }
-    ],
-    friendsId: [
-        {
-            type: mongoose.Types.ObjectId,
-            ref: 'User'
-        }
-    ],
-    groupsId: [
-        {
-            type: mongoose.Types.ObjectId,
-            ref: 'User'
-        }
-    ]
-}, {timestamps: true}
-)
->>>>>>> Stashed changes
+
 
 const UserModel = mongoose.model<UserDto>("User", UserSchema);
 
