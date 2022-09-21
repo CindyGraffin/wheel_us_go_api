09384b1e3b709f43c1ca56bd4a2ce5f11f09032f
CindyGraffin <graffincindy@gmail.com>
2022-09-21 22:59:33 +0200

    fix: createRoomDto

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -2,32 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { userService } from "../service/userService";
 import jwt from "jsonwebtoken";
 
-<<<<<<< Updated upstream
 export class AuthController {
-=======
-export const register = async(req: Request, res: Response, next: NextFunction) => {
-    try {
-        const salt = bcrypt.genSaltSync(10);
-        const hashPassword = bcrypt.hashSync(req.body.password, salt)
-        const newUser = new UserModel({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            email: req.body.email,
-            role: 'user',
-            userImg: 'https://images.unsplash.com/photo-1613318286980-4b3dd8475772?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-            birthday: '2012-04-23T18:25:43.511Z',
-            password: hashPassword,
-            city: req.body.city,
-            outingPart: 0,
-            outingCre: 0
-        })
-        await newUser.save();
-        res.status(200).json(newUser);
-    } catch (error) {
-        next(error);
-    }
-}
->>>>>>> Stashed changes
 
     private service = userService;
 
